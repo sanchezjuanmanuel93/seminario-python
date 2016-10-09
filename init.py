5c9ff5136b339268005afabea4087982cb1f4313
Juan Manuel <sanchez.juanmy@gmail.com>
2016-10-09 11:53:50 -0300

    filtro para el dni

--- init.py
+++ init.py
@@ -1,10 +1,14 @@
+from django.http import HttpResponse
 from flask import Flask
 from flask import render_template
+from flask import jsonify
+import json
 from PersonaDatos import PersonaDatos
 
 app = Flask(__name__)
 
 personaDatos = PersonaDatos()
+
 
 @app.route('/')
 def hello_world():
@@ -14,6 +18,17 @@ def hello_world():
 def personasAll():
     personas = personaDatos.getAll()
     return render_template('persona/personas.html', personas=personas)
+
+
+@app.route('/api/personas/')
+def personasAllAPI():
+    personas = personaDatos.getAll()
+    jsonStr = json.dumps([e.toJSON() for e in personas])
+    return jsonStr
+
+@app.route('/api/personas/<int:dni>')
+def personasGetAPI(dni):
+    return jsonify(personas = personaDatos.get(dni).toJSON())
 
 if __name__ == "__main__":
     app.run(debug=True)
